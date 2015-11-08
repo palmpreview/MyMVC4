220b69dc4a12522b80726814b6e0d8a3f945573f
palmpreview <palmpreview@gmail.com>
2015-11-08 16:15:07 +0700

    First Import

---
+++ MyMVC4/App_Start/BundleConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace MyMVC4.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                "~/Scripts/jquery-1.9.1.js"));
+            bundles.Add(new StyleBundle("~/bundles/bootstrap").Include(
+                "~/Content/bootstrap-theme.css",
+                "~/Content/bootstrap.css"));
+        }
+    }
+}
---
+++ MyMVC4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyMVC4.MvcApplication" Language="C#" %>
